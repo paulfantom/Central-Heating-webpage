931b433e000691052d9d81cf7ddc4e96a856bcce
paulfantom <paulfantom@gmail.com>
2015-03-03 18:38:32 +0100

    webpage - unified templates: solar,circulation,water

--- app/views.py
+++ app/views.py
@@ -29,36 +29,39 @@ def dashboard():
              "heater_status" : 'on',
              "solar_status"  : 'on' }
     return render_template("content/dashboard.html",
-                           title='Home',
+                           title='',
                            user=user,
                            data=data)
 
 @app.route('/status')
 def status():
-    return render_template("content/status.html", title='Status')
+    values = []
+    return render_template("data_rows.html",data=values,title='Status',nav_item='nav-status')
 
 @app.route('/water')
 def water():
-
-    values = [ {'title'  : u'Aktualna temp.',
+    values = [ {'title' : u'Aktualna temperatura',
                 'value' : 44 },
-               {'name'  : ["solar_max"],
-                'value' : [90],
+               {'name'  : "solar_max",
+                'value' : 90,
                 'range' : [30,100],
                 'title' : u'Solar',
-                'desc'  : [u'Ustaw maksymalną temperaturę wody w zbiorniku dla zasilania z kolektora'] },
-               {'name'  : ["heater_max","heater_min"],
-                'value' : [90,30],
-                'info'  : [u'Maximum',
-                           u'Minimum'],
+                'desc'  : u'Ustaw maksymalną temperaturę wody w zbiorniku dla zasilania z kolektora' },
+               {'name'  : "heater_max",
+                'value' : 90,
                 'range' : [30,100],
-                'title' : u'Piec',
-                'desc'  : [u'Ustaw maksymalną temperaturę wody w zbiorniku dla zasilania z pieca',
-                           u'Ustaw minimalną temperaturę wody w zbiorniku dla zasilania z pieca'] } ]
+                'title' : u'Piec (max)',
+                'desc'  : u'Ustaw maksymalną temperaturę wody w zbiorniku dla zasilania z pieca' },
+               {'name'  : "heater_min",
+                'value' : 30,
+                'range' : [30,100],
+                'title' : u'Piec (min)',
+                'desc'  : u'Ustaw minimalną temperaturę wody w zbiorniku dla zasilania z pieca' }]
 
-    return render_template("content/water.html",
-                           title='Water',
-                           data=values)
+    return render_template("data_rows.html",
+                           data=values,
+                           title='Woda',
+                           nav_item='nav-water')
 
 @app.route('/circulation')
 def circulation():
@@ -66,45 +69,75 @@ def circulation():
                 'value' : 40,
                 'range' : [30,100],
                 'unit'  : u'°C',
-                'title' : 'Temperatura pracy',
+                'title' : u'Temperatura pracy',
                 'desc'  : u'Ustaw temperaturę pracy pompy cyrkulacyjnej' },
                {'name'  : "circulation_hyst",
                 'value' : 4,
                 'range' : [0.5,10],
                 'unit'  : u'°C',
-                'title' : 'Histereza pracy',
+                'title' : u'Histereza pracy',
                 'desc'  : u'Ustaw histerezę pracy pompy cyrkulacyjnej' },
                {'name'  : "circulation_solar",
                 'value' : 70,
                 'range' : [30,150],
                 'unit'  : u'°C',
-                'title' : 'Wymagana temperatura kolektora',
+                'title' : u'Wymagana temperatura kolektora',
                 'desc'  : u'Ustaw minimalną temperaturę kolektora dla załączenia cyrkulacji' },
                {'name'  : "time_on",
                 'value' : 30,
                 'range' : [1,300],
                 'unit'  : 's',
-                'title' : 'Czas pracy',
+                'title' : u'Czas pracy',
                 'desc'  : u'Ustaw czas pracy pompy w trybie poboru wody' },
                {'name'  : "time_off",
                 'value' : 30,
                 'range' : [1,180],
                 'unit'  : ' min',
-                'title' : 'Przerwa',
+                'title' : u'Przerwa',
                 'desc'  : u'Ustaw przerwę w pracy pompy w trybie poboru wody' } ]
-
-    return render_template("content/circulation.html",
-                           title='Circulation',
-                           data=values)
+    return render_template("data_rows.html",
+                           data=values,
+                           title="Cyrkulacja",
+                           nav_item='nav-circulation')
 
 @app.route('/heater')
 def heater():
-    return render_template("content/heater.html", title='Heater')
+    return render_template("content/heater.html", title='Piec')
 
 @app.route('/solar')
 def solar():
-    return render_template("content/solar.html", title='Solar')
+    values = [ {'title' : u'Aktualna temperatura',
+                'value' : 10 },
+               {'name'  : "solar_critical",
+                'value' : 130,
+                'range' : [70,200],
+                'unit'  : u'°C',
+                'title' : u'Temperatura krytyczna',
+                'desc'  : u'Ustaw krytyczną temperaturę wyłączenia kolektora' },
+               {'name'  : "solar_tank",
+                'value' : 90,
+                'range' : [0.5,10],
+                'unit'  : u'°C',
+                'title' : u'Temperatura wody',
+                'desc'  : u'Ustaw maksymalną temperaturę wody w zbiorniku dla zasilania z kolektora' },
+               {'name'  : "solar_on",
+                'value' : 4,
+                'range' : [0.5,15],
+                'unit'  : u'°C',
+                'title' : u'Delta załączenia',
+                'desc'  : u'Ustaw wartość różnicy temperatur powodującą załączenie układu solarnego' },
+               {'name'  : "solar_off",
+                'value' : 8,
+                'range' : [0.5,15],
+                'unit'  : u'°C',
+                'title' : u'Delta wyłączenia',
+                'desc'  : u'Ustaw wartość różnicy temperatur powodującą wyłączenie układu solarnego' }]
+
+    return render_template("data_rows.html",
+                           data=values,
+                           title="Solar",
+                           nav_item='nav-solar')
 
 @app.route('/options')
 def options():
-    return render_template("content/options.html", title='Options')
+    return render_template("content/options.html", title='Opcje')
