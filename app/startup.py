8306affc492424a78163def4dd75c9c74323ecbb
Paul <paulfantom@gmail.com>
2015-10-29 18:13:20 +0100

    added startup script

---
+++ app/startup.py
@@ -0,0 +1,14 @@
+
+from app import app, db, bcrypt
+from app.models import User
+
+@app.before_first_request
+def startup():
+    db.create_all()
+    user = User.query.filter(User.username == 'admin').first()
+    if not user:
+        print("Creating user")
+        user = User(username = 'admin', 
+                    password = 'password')
+        db.session.add(user)
+    db.session.commit()
