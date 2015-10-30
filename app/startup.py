d864d4ea9dd469b688e311646dedda7d8f3deada
Paul <paulfantom@gmail.com>
2015-10-30 21:16:30 +0100

    flash startup errors

--- app/startup.py
+++ app/startup.py
@@ -2,6 +2,8 @@
 from app import app, db, bcrypt
 from app.core.models import User
 from sqlalchemy.exc import OperationalError
+from flask import flash
+from flask.ext.babel import gettext
 
 @app.before_first_request
 def startup():
@@ -9,9 +11,10 @@ def startup():
         db.create_all()
         user = User.query.filter(User.username == 'admin').first()
     except OperationalError:
+        flash(gettext("Cannot connect to database"))
         return
     if not user:
-        print("Creating user")
+        flash(gettext("Created user")+" 'admin'")
         user = User(username = 'admin', 
                     password = 'password')
         db.session.add(user)
