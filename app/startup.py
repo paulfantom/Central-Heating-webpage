32816fb1cc44328ecdcf94ec2317686432468619
Paul <paulfantom@gmail.com>
2015-10-30 21:11:01 +0100

    handle OperationalError when creating db

--- app/startup.py
+++ app/startup.py
@@ -1,11 +1,15 @@
 
 from app import app, db, bcrypt
 from app.core.models import User
+from sqlalchemy.exc import OperationalError
 
 @app.before_first_request
 def startup():
-    db.create_all()
-    user = User.query.filter(User.username == 'admin').first()
+    try:
+        db.create_all()
+        user = User.query.filter(User.username == 'admin').first()
+    except OperationalError:
+        return
     if not user:
         print("Creating user")
         user = User(username = 'admin', 
