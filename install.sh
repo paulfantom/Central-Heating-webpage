70aa9eceb9176068eca0077fb5f818614388c484
Paul <paulfantom@gmail.com>
2015-10-30 22:05:47 +0100

    password setter in instalation script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+PASSWORD = "$1"
+
 sudo apt-get install -y python-virtualenv libffi-dev python-dev
 
 virtualenv venv
 source venv/bin/activate
 pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt
+
+ed -i -e "s/MYSQL_PASS = ''/MYSQL_PASS = '$PASSWORD'/" config.py
