4f631457402c45301ddf15846938692f5530aa85
Paul <paulfantom@gmail.com>
2015-11-03 22:33:26 +0100

    added uwsgi

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y python-virtualenv libffi-dev python-dev
 
 virtualenv venv
 source venv/bin/activate
-pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt
+pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt uwsgi
 
 sed -i -e "s/MYSQL_PASS = ''/MYSQL_PASS = '$PASSWORD'/" config.py
