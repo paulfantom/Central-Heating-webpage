5a275d3f1b033185c3095b162f2d745ad69f8ee6
Paul <paulfantom@gmail.com>
2015-11-04 09:41:04 +0100

    remove uwsgi python package

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y python-virtualenv libffi-dev python-dev
 
 virtualenv venv
 source venv/bin/activate
-pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt uwsgi
+pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt
 
 sed -i -e "s/MYSQL_PASS = ''/MYSQL_PASS = '$PASSWORD'/" config.py
