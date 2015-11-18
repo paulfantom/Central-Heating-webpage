9e33d06293b4f7bf0b0a3770823fc43534c19589
Paul <paulfantom@gmail.com>
2015-11-18 19:44:10 +0100

    deactivate venv on exit

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ eval $SUDO apt-get install -y python-virtualenv libffi-dev python-dev
 virtualenv venv
 source venv/bin/activate
 pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt
-pip install markupsafe pytz 
 
 sed -i -e "s/MYSQL_PASS = ''/MYSQL_PASS = '$PASSWORD'/" config.py
+
+deactivate
+exit 0
