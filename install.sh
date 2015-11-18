b33fc2873571e2c105ada58fb23a4691bffdc5da
Paul <paulfantom@gmail.com>
2015-11-18 19:27:47 +0100

    ability to install as root

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 PASSWORD="$1"
 
-sudo apt-get install -y python-virtualenv libffi-dev python-dev
+if [ $(id -u) -eq 0 ]; then
+  SUDO=""
+else
+  SUDO="sudo"
+fi
+
+eval $SUDO apt-get install -y python-virtualenv libffi-dev python-dev
 
 virtualenv venv
 source venv/bin/activate
 pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt
+pip install markupsafe pytz 
 
 sed -i -e "s/MYSQL_PASS = ''/MYSQL_PASS = '$PASSWORD'/" config.py
