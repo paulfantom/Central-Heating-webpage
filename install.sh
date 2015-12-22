5e17b4d154d008d5346a4f44ba3353ff4f87a879
Paul <paulfantom@gmail.com>
2015-12-22 11:05:58 +0100

    change CSRF key on install

--- install.sh
+++ install.sh
@@ -13,7 +13,9 @@ virtualenv venv
 source venv/bin/activate
 pip install flask{,-login,-wtf,-sqlalchemy,-babel,-bcrypt} wtforms sqlalchemy jinja2 pymysql paho-mqtt
 
+RANDOM_KEY=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n1)
 sed -i -e "s/MYSQL_PASS = ''/MYSQL_PASS = '$PASSWORD'/" config.py
+sed -i -e "s/SECRET_KEY = 'you-will-never-guess'/SECRET_KEY = '$RANDOM_KEY'/" config.py
 
 deactivate
 exit 0
